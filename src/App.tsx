1a1652217e4b675d4a991f12e8b14c79fd33e47d
Pablo Poggio <pablopoggiog@gmail.com>
2021-11-17 20:00:45 -0300

    Create getLatestBlocks to start fetching the latest 10 blocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
+import { useCallback, useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { BlockWithTransactions } from "@ethersproject/abstract-provider";
 import styled from "styled-components";
 import { BlocksList } from "./components";
 
 const App = () => {
+  const [latestBlocks, setLatestBlocks] = useState<BlockWithTransactions[]>([]);
+
+  const getLatestBlocks = useCallback(async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+
+        const latestBlockNumber = await provider.getBlockNumber();
+
+        const blocks = [];
+
+        for (
+          let currentBlock = latestBlockNumber;
+          blocks.length < 10;
+          currentBlock--
+        ) {
+          const transactions = await provider.getBlockWithTransactions(
+            currentBlock
+          );
+
+          blocks.push(transactions);
+        }
+
+        setLatestBlocks(blocks);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getLatestBlocks();
+  }, [getLatestBlocks]);
+
+  useEffect(() => {
+    console.log({ latestBlocks });
+  }, [latestBlocks]);
+
   return (
     <Container>
       <BlocksList />
