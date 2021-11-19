b27dee32db71ea2f661452a2c2d1645c03f98dd1
Pablo Poggio <pablopoggiog@gmail.com>
2021-11-19 13:20:57 -0300

    Create TransactionModal

---
+++ src/components/TransactionModal/index.tsx
@@ -0,0 +1,46 @@
+import { FunctionComponent } from "react";
+import { TransactionResponse } from "@ethersproject/abstract-provider";
+import { getBigNumber } from "src/utils";
+import {
+  Background,
+  CloseButton,
+  Container,
+  Transaction,
+  Field,
+  Label,
+} from "./styles";
+
+interface TransactionModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  transaction: TransactionResponse;
+}
+
+export const TransactionModal: FunctionComponent<TransactionModalProps> = ({
+  isOpen,
+  onClose,
+  transaction,
+}) => {
+  return (
+    <Background isOpen={isOpen}>
+      <Container>
+        <CloseButton onClick={onClose}>x</CloseButton>
+        <Transaction>
+          <Label>Hash:</Label>
+          <Field>{transaction.hash}</Field>
+          <Label>From:</Label>
+          <Field>{transaction.from}</Field>
+          {transaction.to && (
+            <>
+              {" "}
+              <Label>To:</Label>
+              <Field>{transaction.to}</Field>
+            </>
+          )}
+          <Label>Value Sent:</Label>
+          <Field>{getBigNumber(transaction.value)}</Field>
+        </Transaction>
+      </Container>
+    </Background>
+  );
+};
