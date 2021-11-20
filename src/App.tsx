586742755a0569fab7d7684ad47e74a7f2aceb93
Pablo Poggio <pablopoggiog@gmail.com>
2021-11-20 13:04:40 -0300

    Add missing try/catchs

--- src/App.tsx
+++ src/App.tsx
@@ -44,41 +44,49 @@ const App = () => {
   }, [provider]);
 
   const subscribeToNewBlocks = useCallback(async () => {
-    if (provider && latestBlocks.length) {
-      provider.on("block", async (blockNumber) => {
-        const newBlock = await provider.getBlockWithTransactions(blockNumber);
+    try {
+      if (provider && latestBlocks.length) {
+        provider.on("block", async (blockNumber) => {
+          const newBlock = await provider.getBlockWithTransactions(blockNumber);
 
-        // Update the blocks leaving in first place the new block, followed by the rest of pre-existing blocks and removing the oldest one.
-        setLatestBlocks((blocks) => {
-          // I've been testing for a while and there seems to be an issue with this subscription, from time to time it receives
-          // an event for the same block twice, duplicating the block in the list.
-          // Usually I'd have this validation outside of the setState, but if I used directly latestBlocks (outside of setLatestBlocks) I'd have to include
-          // it as dependency of the useCallback, causing subscribeToNewBlocks to be re-created and re-run in its useEffect every time a new block is added.
-          //  My 2 options were: 1. Ignoring depdendencies, going against a React's principle, or
-          // 2. Putting logic inside the setState (setLatestBlocks), that looks horrible to me but I don't remember to have read anything against it.
-          // Happy to know a better solution (and there might be plenty more than 1) if someone finds it!
-          const existingNumbers = blocks.map((block) => block.number);
+          // Update the blocks leaving in first place the new block, followed by the rest of pre-existing blocks and removing the oldest one.
+          setLatestBlocks((blocks) => {
+            // I've been testing for a while and there seems to be an issue with this subscription, from time to time it receives
+            // an event for the same block twice, duplicating the block in the list.
+            // Usually I'd have this validation outside of the setState, but if I used directly latestBlocks (outside of setLatestBlocks) I'd have to include
+            // it as dependency of the useCallback, causing subscribeToNewBlocks to be re-created and re-run in its useEffect every time a new block is added.
+            //  My 2 options were: 1. Ignoring depdendencies, going against a React's principle, or
+            // 2. Putting logic inside the setState (setLatestBlocks), that looks horrible to me but I don't remember to have read anything against it.
+            // Happy to know a better solution (and there might be plenty more than 1) if someone finds it!
+            const existingNumbers = blocks.map((block) => block.number);
 
-          if (!existingNumbers.includes(blockNumber))
-            return [newBlock, ...blocks.slice(0, -1)];
-          else return [...blocks];
+            if (!existingNumbers.includes(blockNumber))
+              return [newBlock, ...blocks.slice(0, -1)];
+            else return [...blocks];
+          });
         });
-      });
+      }
+    } catch (error) {
+      console.log(error);
     }
   }, [provider, latestBlocks.length]);
 
   const connectWithProvider = useCallback(() => {
-    const { ethereum } = window;
+    try {
+      const { ethereum } = window;
 
-    if (ethereum) {
-      setProvider(
-        new ethers.providers.WebSocketProvider(
-          `${INFURA_WEBSOCKETS_URL}${INFURA_PROJECT_ID}`,
-          INFURA_NETWORK
-        )
-      );
-    } else {
-      console.log("Ethereum object doesn't exist!");
+      if (ethereum) {
+        setProvider(
+          new ethers.providers.WebSocketProvider(
+            `${INFURA_WEBSOCKETS_URL}${INFURA_PROJECT_ID}`,
+            INFURA_NETWORK
+          )
+        );
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
     }
   }, []);
 
