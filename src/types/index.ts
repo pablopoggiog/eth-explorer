461d6f829bde845c15e39319e3367600220d7a92
Pablo Poggio <pablopoggiog@gmail.com>
2021-11-20 13:18:22 -0300

    Move some types to the types directory

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,17 @@
+import { ethers } from "ethers";
+import { BlockWithTransactions as EthersBlockWithTransactions } from "@ethersproject/abstract-provider";
+
 declare global {
-    interface Window {
-      ethereum?: any;
-    }
+  interface Window {
+    ethereum?: any;
   }
-  
-  export {};
-  
+}
+
+export type WebSocketProvider = ethers.providers.WebSocketProvider;
+
+export type BlockWithTransactions = EthersBlockWithTransactions;
+
+export interface EthereumContextReturn {
+  latestBlocks: any[];
+  userAddress: string;
+}
