6660fd94c51f3daa58e5d228a128f587ed878490
Pablo Poggio <pablopoggiog@gmail.com>
2021-11-19 21:44:24 -0300

    Update the blocks list in real time

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import { ethers } from "ethers";
 import { BlockWithTransactions } from "@ethersproject/abstract-provider";
 import styled from "styled-components";
 import { BlocksList } from "./components";
+import {
+  INFURA_WEBSOCKETS_URL,
+  INFURA_PROJECT_ID,
+  INFURA_NETWORK,
+} from "./constants";
+
+type WebSocketProvider = ethers.providers.WebSocketProvider;
 
 const App = () => {
   const [latestBlocks, setLatestBlocks] = useState<BlockWithTransactions[]>([]);
+  const [provider, setProvider] = useState<WebSocketProvider | null>(null);
 
   const getLatestBlocks = useCallback(async () => {
     try {
-      const { ethereum } = window;
-
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-
+      if (provider) {
         const latestBlockNumber = await provider.getBlockNumber();
 
         const blocks = [];
@@ -23,33 +27,73 @@ const App = () => {
           blocks.length < 10;
           currentBlock--
         ) {
-          const transactions = await provider.getBlockWithTransactions(
+          const newBlock = await provider.getBlockWithTransactions(
             currentBlock
           );
 
-          blocks.push(transactions);
+          blocks.push(newBlock);
         }
 
         setLatestBlocks(blocks);
-      } else {
-        console.log("Ethereum object doesn't exist!");
       }
     } catch (error) {
       console.log(error);
     }
+  }, [provider]);
+
+  const subscribeToNewBlocks = useCallback(async () => {
+    if (provider && latestBlocks.length) {
+      provider.on("block", async (blockNumber) => {
+        const newBlock = await provider.getBlockWithTransactions(blockNumber);
+
+        // Update the blocks leaving in first place the new block, followed by the rest of pre-existing blocks and removing the oldest one.
+        setLatestBlocks((blocks) => {
+          // I've been testing for a while and there seems to be an issue with this subscription, from time to time it receives
+          // an event for the same block twice, duplicating the block in the list.
+          // Usually I'd have this validation outside of the setState, but if I used directly latestBlocks (outside of setLatestBlocks) I'd have to include
+          // it as dependency of the useCallback, causing subscribeToNewBlocks to be re-created and re-run in its useEffect every time a new block is added.
+          const existingNumbers = blocks.map((block) => block.number);
+
+          if (!existingNumbers.includes(blockNumber))
+            return [newBlock, ...blocks.slice(0, -1)];
+          else return [...blocks];
+        });
+      });
+    }
+  }, [provider, latestBlocks.length]);
+
+  const connectWithProvider = useCallback(() => {
+    const { ethereum } = window;
+
+    if (ethereum) {
+      setProvider(
+        new ethers.providers.WebSocketProvider(
+          `${INFURA_WEBSOCKETS_URL}${INFURA_PROJECT_ID}`,
+          INFURA_NETWORK
+        )
+      );
+    } else {
+      console.log("Ethereum object doesn't exist!");
+    }
   }, []);
 
+  useEffect(() => {
+    connectWithProvider();
+  }, [connectWithProvider]);
+
+  // Fetch the initial 10 blocks, only if there's already a provider connected (verified internally)
   useEffect(() => {
     getLatestBlocks();
   }, [getLatestBlocks]);
 
+  // Listen to new blocks, to update the list each time a new block is mined
   useEffect(() => {
-    console.log({ latestBlocks });
-  }, [latestBlocks]);
+    subscribeToNewBlocks();
+  }, [subscribeToNewBlocks]);
 
   return (
     <Container>
-      <BlocksList blocks={latestBlocks} />
+      {latestBlocks.length && <BlocksList blocks={latestBlocks} />}
     </Container>
   );
 };
